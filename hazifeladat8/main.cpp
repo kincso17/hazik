3d7df7da31126c60a2ce88711e6efd342e853e68
kincso17 <91244064+kincso17@users.noreply.github.com>
2023-04-26 16:14:03 +0300

    Add files via upload

--- hazifeladat8/main.cpp
+++ hazifeladat8/main.cpp
@@ -4,33 +4,29 @@ using namespace std;
 
 int main()
 {
-    int t[100];
+    int szam;
     ifstream in("atestat.in");
     ofstream out("atestat.out");
-    int i=0;
-    while(in>>t[i]){
-        i++;
-    }
-    bool van=true;
-    for(int j=0; j<i; j++){
-        int szamjegy;
-        int ideiglenes=t[j];
-        bool paros=true;
-        while(ideiglenes>0){
-            szamjegy=ideiglenes%10;
-            ideiglenes=ideiglenes/10;
-            if(szamjegy%2==1){
-                paros=false;
+    int i=0,a;
+    while(in>>szam){
+        bool van=true;
+        a=szam;
+        while(szam!=0){
+            if(szam%2!=0){
+                van=false;
+                break;
             }
+            szam=szam/10;
+           }
+           if(van==true){
+             out << a << " ";
+             i++;
+           }
 
-        }
-        if(paros=true){
-            out << t[j] <<" ";
-            van=false;
-        }
     }
-    if(van=false){
-        out<<"nem létezik";
+    if(i==0){
+        out << "nem létezik";
     }
+
     return 0;
 }
